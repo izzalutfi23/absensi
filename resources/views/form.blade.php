07de6a9a50aea0719bba7b6699070990cb5cfcd4
izzalutfi23 <izzalutfi045@gmail.com>
2022-11-15 10:57:59 +0700

    update

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<title>Form Ketidakhadiran</title>
+	<title>Form Absensi</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
